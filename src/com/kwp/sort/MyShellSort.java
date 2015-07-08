f953e451929430808b3bf957a8c681806596e870
Weiping <Weiping@115.154.122.135>
2015-07-08 23:45:38 +0800

    Shell排序的基本实现

--- src/com/kwp/sort/MyShellSort.java
+++ src/com/kwp/sort/MyShellSort.java
@@ -1,5 +1,8 @@
 /**
- * 
+ * Shell排序：
+ * 	分组（缩小增量）插入排序算法，不稳定
+ * 	关键：将待排数据进行分组，不断的使用插入排序
+ * 	概念：h序列  h=3*h+1(该序列从1开始) 1，4，
  */
 package com.kwp.sort;
 
@@ -24,13 +27,39 @@ public class MyShellSort extends MyAbstractSort implements MySort {
 		// TODO Auto-generated constructor stub
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see com.kwp.sort.MyAbstractSort#sort()
 	 */
 	@Override
 	public void sort() {
-		// TODO Auto-generated method stub
+		shellSort(list);
 
+	}
+
+	private void shellSort(int[] data) {
+		// 计算出最大的h值
+		int h = 1;
+		while (h <= data.length / 3) {
+			h = h * 3 + 1;
+		}
+		while (h > 0) {
+			for (int i = h; i < data.length; i += h) {
+				if (data[i] < data[i - h]) {
+					int tmp = data[i];
+					int j = i - h;
+					while (j >= 0 && data[j] > tmp) {
+						data[j + h] = data[j];
+						j -= h;
+					}
+					data[j + h] = tmp;
+
+				}
+			}
+			// 计算出下一个h值
+			h = (h - 1) / 3;
+		}
 	}
 
 }
--- src/com/kwp/test/TestSort.java
+++ src/com/kwp/test/TestSort.java
@@ -11,20 +11,20 @@ public class TestSort {
 		// int[] list = {1,2,33,4,5,6,77,3,22};
 
 		Random random = new Random();
-		int num = 200;
+		int num = 20;
 		int a[] = new int[num];
 		for (int i = 0; i < num; i++) {
 			a[i] = random.nextInt(num * 10);
-			//System.out.print(a[i]+"  ");
+			System.out.print(a[i]+"  ");
 		}
 
 		long startTime = System.currentTimeMillis();
-		MySort sort = new MyQuickSort(a);
+		MySort sort = new MyShellSort(a);
 		sort.sort();
 		long endTime = System.currentTimeMillis(); // 获取结束时间
 
 		System.out.println("\n排序运行时间： " + (endTime - startTime) + "ms");
-		//sort.print();
+		sort.print();
 
 	}
 
