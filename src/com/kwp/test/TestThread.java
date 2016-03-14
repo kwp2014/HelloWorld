129c6b7f2d4c8f17319088ca48e3ef342dd390da
Weiping <Weiping@KWP-PC>
2016-03-14 11:30:41 +0800

    线程学习

--- src/com/kwp/test/TestThread.java
+++ src/com/kwp/test/TestThread.java
@@ -8,7 +8,7 @@ public class TestThread implements Runnable {
 	public void run(){
 		for(;num<1000;num++){
 			System.out.print(Thread.currentThread().toString());
-			System.out.println(": " + num);
+			System.out.println("Ïß³Ì: " + num);
 		}
 	}
 	
